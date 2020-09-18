c9cfd5a71722fd1388729678f9c10db271a610b1
ABOUBACAR ALKA, Mahamadou Salissou <alkasalissou@gmail.com>
2020-09-18 22:16:43 +0200

    adjust some dimensions

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
   homePage: {
     position: 'fixed',
     width: '100%',
-    height: '100%',
+    height: '95%',
     textAlign: 'center',
     display: 'flex',
     flexGrow: 1,
