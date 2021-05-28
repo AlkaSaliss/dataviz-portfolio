58433d9e4a9d9592a125fe42d89e172919a327bd
AlkaSaliss <alkasalissou@gmail.com>
2021-05-29 01:36:00 +0200

    Updates

---
+++ precache-manifest.3a35cc053c3e99f89573a3bf27061861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1cef163f1378468c6a1706beec3d12a",
+    "url": "/alkai/index.html"
+  },
+  {
+    "revision": "9950806e8ef0b4cfdfe6",
+    "url": "/alkai/static/css/main.652d0833.chunk.css"
+  },
+  {
+    "revision": "836bf671be1a3aba24d7",
+    "url": "/alkai/static/js/2.ee386f0a.chunk.js"
+  },
+  {
+    "revision": "1910a5c34c0939942633421465946d76",
+    "url": "/alkai/static/js/2.ee386f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9950806e8ef0b4cfdfe6",
+    "url": "/alkai/static/js/main.56fc9abf.chunk.js"
+  },
+  {
+    "revision": "1af1d8b94cacf2b76285",
+    "url": "/alkai/static/js/runtime-main.85609103.js"
+  }
+]);
